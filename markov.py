8bc1ae541f5394f103636390e9135b50a384d6ef
Hackbright Student <info@hackbrightacademy.com>
2016-01-13 16:42:57 -0800

    Username added

--- markov.py
+++ markov.py
@@ -83,7 +83,7 @@ def make_text(chains):
 
     return make_text(chains)
 
-
+# username @ka_markov
 def tweet(chains):
     # Use Python os.environ to get at environmental variables
     # Note: you must run `source secrets.sh` before running this file
@@ -95,9 +95,10 @@ def tweet(chains):
         access_token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET'])
     user_creds = api.VerifyCredentials()
     statuses = api.GetUserTimeline(screen_name=user_creds.screen_name)
-    print statuses[0].text #statuses[0].coordinates
+    print statuses[0].text, statuses[0].coordinates
 
-    status = api.PostUpdate(make_text(chains) + ' #Hack13right')
+    status = api.PostUpdate(make_text(chains) + ' #Hack13right', latitude=37.7886334,
+                            longitude=-122.4136639)
 
 
 # print make_text(make_chains(open_and_read_file('gettysburg.txt')))
